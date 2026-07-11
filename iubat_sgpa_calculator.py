c2f4d958911263b20846677814e2a3978d378674
Sadik Hasan Mubin <92442164+mubiny87@users.noreply.github.com>
2026-07-11 13:27:01 +0600

    Initial commit - IUBAT SGPA Calculator v2.0

---
+++ iubat_sgpa_calculator.py
@@ -0,0 +1,67 @@
+TotalCourses = int(input("How many courses this semester? "))
+TotalCredits = 0
+TotalQualityPoints = 0
+
+for i in range(TotalCourses):
+    print(f"\n========== COURSE {i+1} ==========")
+
+    C_Name = input("Course Name: ")
+    Credit = float(input("Credit: "))
+
+    MID = float(input("Mid out of 100: "))
+    CT = float(input("CT out of 100: "))
+    ATT = float(input("Attendance out of 5: "))
+    ASS = float(input("Assignment out of 10: "))
+    FINAL = float(input("Final out of 100: "))
+
+    CG = (MID / 4) + (CT / 10) + ATT + ASS + (FINAL / 2)
+
+    if CG >= 80:
+        LetterGrade = "A+"
+        GradePoint = 4.00
+    elif CG >= 75:
+        LetterGrade = "A"
+        GradePoint = 3.75
+    elif CG >= 70:
+        LetterGrade = "A-"
+        GradePoint = 3.50
+    elif CG >= 65:
+        LetterGrade = "B+"
+        GradePoint = 3.25
+    elif CG >= 60:
+        LetterGrade = "B"
+        GradePoint = 3.00
+    elif CG >= 55:
+        LetterGrade = "B-"
+        GradePoint = 2.75
+    elif CG >= 50:
+        LetterGrade = "C+"
+        GradePoint = 2.50
+    elif CG >= 45:
+        LetterGrade = "C"
+        GradePoint = 2.25
+    elif CG >= 40:
+        LetterGrade = "D"
+        GradePoint = 2.00
+    else:
+        LetterGrade = "F"
+        GradePoint = 0.00
+
+    QualityPoints = Credit * GradePoint
+
+    print("\n========== COURSE RESULT ==========")
+    print(f"Course Name         : {C_Name}")
+    print(f"Course Credit       : {Credit}")
+    print(f"Overall Marks       : {CG:.2f}")
+    print(f"Letter Grade        : {LetterGrade}")
+    print(f"Grade Point         : {GradePoint:.2f}")
+    print(f"Quality Points      : {QualityPoints:.2f}")
+
+    TotalCredits += Credit
+    TotalQualityPoints += QualityPoints
+
+SGPA = TotalQualityPoints / TotalCredits
+
+print("\n========== SEMESTER RESULT ==========")
+print(f"Total Credits        : {TotalCredits}")
+print(f"Semester GPA (SGPA)  : {SGPA:.2f}")
